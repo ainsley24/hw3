9c98d9eb50111277aab4b85aed985dfc83c5c86c
Ainsley Campbell <ainsley.campbell@kellogg.northwestern.edu>
2023-05-19 16:26:24 +0000

    finished

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+    @place = Place.find_by({ "id" => params["place_id"] })
+    @post["place_id"] = @place["id"]
+  end
+
+  def create
+    @post = Post.new
+    @post["activity"] = params["post"]["activity"]
+    @post["posted_on"] = params["post"]["posted_on"]
+    @post.save
+    redirect_to "/places/#{@post["place_id"]}"
+  end
 end
